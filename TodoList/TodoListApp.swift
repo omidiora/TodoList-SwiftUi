20eae80ef841337fd5d56245f94f2fb65dbd0009
omidiora <omidioraemmanuel@gmail.com>
2026-01-16 08:28:10 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Omidiora on 16/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
